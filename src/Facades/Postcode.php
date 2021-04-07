9f81233162e8195748cd14235d154937053e2262
dnwjn <hello@dnwjn.dev>
2021-04-07 15:04:12 +0200

    Implemented a facade for easier access to the service

---
+++ src/Facades/Postcode.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustSteveKing\LaravelPostcodes\Facades;
+
+use Illuminate\Support\Facades\Facade;
+use JustSteveKing\LaravelPostcodes\Service\PostcodeService;
+
+/** @see \JustSteveKing\LaravelPostcodes\Service\PostcodeService */
+class Postcode extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return PostcodeService::class;
+    }
+}
